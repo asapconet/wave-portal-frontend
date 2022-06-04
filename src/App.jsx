45ed533adbb13c6855b50af9fb6648a709ec183e
asapconet <aaronrickymeek@gmail.com>
2022-06-04 05:36:57 +0100

    UI fix

--- src/App.jsx
+++ src/App.jsx
@@ -192,59 +192,72 @@ export default function App() {
   }, []);
 
   return (
-    <div className="mainContainer">
-      <div className="dataContainer">
-        <div className="header">👋 Hey there!</div>
-        <div className="bio">
-          I am Asap and I work with web2 & 3 simultaniosly that's pretty cool
-          right? Connect your Ethereum wallet and holla at me!
-        </div>
-        <div className="msg--wave">
-          <input
-            id="message"
-            type="text"
-            placeholder={"spill out here ~kiki"}
-            className="msg--box"
-            ref={enteredMessageRef}
-          />
-          <button className="waveButton" onClick={wave}>
-            Wave at Me
-          </button>
-        </div>
-        {!currentAccount && (
-          <button className="waveButton" onClick={walletConnect}>
-            Connect Wallet
-          </button>
-        )}
-        <span className="bio">
-          {minning ? (
-            <p>Your wave dropping in sec...</p>
-          ) : (
+    <>
+      {currentAccount && (
+        <button className="waveButton" onClick={walletConnect}>
+          Connect Wallet
+        </button>
+      )}
+      <div className="mainContainer">
+        <div className="dataContainer">
+          <div className="header">👋 Hey there!</div>
+          <div className="bio">
+            Hi my name is Asap and I work with web2 & 3 simultaniosly that's
+            pretty cool yhh. right? Connect your Ethereum wallet and holla at
+            me!
             <p>
-              Big thanks for prior waver!!! <br /> It's ok to wave more ;-)
+              So you can send me pretty much anything [link to your fav movie, a
+              Message... something sweet] a stand a chance of getting 0.0001eth
+              with love from me.
             </p>
+          </div>
+          <div className="msg--wave">
+            <input
+              id="message"
+              type="text"
+              placeholder={"spill out here ~kiki"}
+              className="msg--box"
+              ref={enteredMessageRef}
+            />
+            <button className="waveButton" onClick={wave}>
+              Wave at Me
+            </button>
+          </div>
+          {!currentAccount && (
+            <button className="waveButton" onClick={walletConnect}>
+              Connect Wallet
+            </button>
           )}
-          <p>{waved && count + " waves in the bag so far..."}</p>
-        </span>
-        <span />
-        {allWaves.map((wave, index) => {
-          return (
-            <div
-              key={index}
-              className="msg--card"
-              styles={{
-                backgroundColor: "OldLace",
-                marginTop: "16px",
-                padding: "8px",
-              }}
-            >
-              <div>Address:{wave.address}</div>
-              <div>Time: {wave.timestamp.toString()}</div>
-              <div>Message: {wave.message}</div>
-            </div>
-          );
-        })}{" "}
+          <span className="bio">
+            {minning ? (
+              <p>Your wave dropping in sec...</p>
+            ) : (
+              <p>
+                Big thanks for prior waver!!! <br /> It's ok to wave more ;-)
+              </p>
+            )}
+            <p>{waved && count + " waves in the bag so far..."}</p>
+          </span>
+          <span />
+          {allWaves.map((wave, index) => {
+            return (
+              <div
+                key={index}
+                className="msg--card"
+                styles={{
+                  backgroundColor: "OldLace",
+                  marginTop: "16px",
+                  padding: "8px",
+                }}
+              >
+                <div>Address:{wave.address}</div>
+                <div>Time: {wave.timestamp.toString()}</div>
+                <div>Message: {wave.message}</div>
+              </div>
+            );
+          })}{" "}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
