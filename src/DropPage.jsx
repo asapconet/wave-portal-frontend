64a9b30af1160a370633fc97c220b8f4ad0b74cc
asapconet <aaronrickymeek@gmail.com>
2022-06-14 15:37:32 +0100

    Updated landing page to connect wallet

--- src/DropPage.jsx
+++ src/DropPage.jsx
@@ -1,21 +1,44 @@
 import React from "react";
+// import { ethers } from "ethers";
 import "./styles/DropPage.css";
 
 const DropPage = () => {
+  const [currentAccount, setCurrentAccount] = React.useState("");
+
+  // wallet connection logic
+  const walletConnect = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get your metamask extention connected to browser");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <div className="quick--nav">
-        <button className="waveButton" onClick={null}>
-          <b>Connect Wallet</b>
-        </button>
+        {currentAccount && (
+          <button className="waveButton" onClick={walletConnect}>
+            <b>Connect Wallet</b>
+          </button>
+        )}
       </div>
       <div className="main--page">
         <h3>WELCOME TO MY SMART CONTRACT WAVER PROJECT</h3>
         <p className="">
           {" "}
-          Hi my name is <b>Asap!</b>
+          Hey I go by the name <b>Asap!</b>
           <br /> Wanna stand a chance to win some eth?
-          <br />
+          <br className="brakes"/>
           Connect your Ethereum wallet and holla at me!
         </p>
       </div>
